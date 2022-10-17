7faadc0fe59fb2c7dafbec40b3496be4187cbbc9
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2022-10-17 15:52:17 +0200

    naming colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     boxShadow: {
-      real: "0 1px 6px rgb(32 33 36 / 28%)",
-      real1: "0 1px 1px rgb(0 0 0 / 10%)",
+      shadow1: "0 1px 1px rgb(0 0 0 / 10%)",
+      shadow2: "0 1px 6px rgb(32 33 36 / 28%)",
     },
 
     fontFamily: {
@@ -12,6 +12,15 @@ module.exports = {
     },
     extend: {
       colors: {
+        "real-white": "#ffff",
+        "primary-white": "#dadce0",
+        "secondary-white": "#f8f9fa",
+        "tertiary-white": "#dfe1e5",
+        "secondary-gray": "#9aa0a6",
+        "tertiary-gray": "#3c4043",
+        "primary-gray": "#f2f2f2",
+        "dark-gray": "#70757a",
+        "light-gray": "#d5d7d8",
         "primary-hover": "rgba(223,225,229,0)",
       },
     },
